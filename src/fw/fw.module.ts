7ef329cc9f42af964d643a290341b5b2b7fecdb5
ArminAg <armin.ag@outlook.com>
2017-07-02 13:58:21 +0100

    Build a Screen Service for Responsive Features

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,12 +1,14 @@
-import { StatusBarComponent } from './status-bar/status-bar.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FrameworkConfigService } from './services/framework-config.service';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { ContentComponent } from './content/content.component';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+import { FrameworkConfigService } from './services/framework-config.service';
+import { ScreenService } from './services/screen.service';
 
 @NgModule({
   imports: [
@@ -20,7 +22,8 @@ import { FrameworkBodyComponent } from './framework-body/framework-body.componen
     StatusBarComponent
   ],
   providers: [
-    FrameworkConfigService
+    FrameworkConfigService,
+    ScreenService
   ],
   exports: [
     FrameworkBodyComponent
