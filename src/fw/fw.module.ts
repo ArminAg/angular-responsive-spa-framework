dcbefdcde9eb75cf4f3e2f1e3c71ae54161781c6
ArminAg <armin.ag@outlook.com>
2017-07-02 16:49:39 +0100

    Add Menu Component

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -6,6 +6,7 @@ import { ContentComponent } from './content/content.component';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { MenuComponent } from './menus/menu/menu.component';
 
 import { FrameworkConfigService } from './services/framework-config.service';
 import { ScreenService } from './services/screen.service';
@@ -25,7 +26,8 @@ import { ScreenLargeDirective } from './directives/screen-large.directive';
     TopBarComponent,
     StatusBarComponent,
     ScreenLargeDirective,
-    ScreenBelowLargeDirective
+    ScreenBelowLargeDirective,
+    MenuComponent
   ],
   providers: [
     FrameworkConfigService,
